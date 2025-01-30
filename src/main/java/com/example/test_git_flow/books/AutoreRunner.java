03a5bc254c6b801d55d0ffe6efc4627a398de9fc
mauro_larese <mauro.larese@omnys.com>
2025-01-30 22:15:20 +0100

    add Book model and AutoreRunner for command line execution

---
+++ src/main/java/com/example/test_git_flow/books/AutoreRunner.java
@@ -0,0 +1,19 @@
+package com.example.test_git_flow.books;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AutoreRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        Book book = new Book();
+        book.setTitle("Spring Boot in Action");
+        book.setIsbn("9781617292545");
+
+        System.out.println("--------------------------" );
+        System.out.println("Book: " + book);
+
+    }
+}
