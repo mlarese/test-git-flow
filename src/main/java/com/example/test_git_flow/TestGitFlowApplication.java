07930e9a6bb684b85cd3e5878094ff9d9fa96a49
mauro_larese <mauro.larese@omnys.com>
2025-01-30 22:08:39 +0100

    Initial commit

---
+++ src/main/java/com/example/test_git_flow/TestGitFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_git_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_git_flow/TestGitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_git_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
