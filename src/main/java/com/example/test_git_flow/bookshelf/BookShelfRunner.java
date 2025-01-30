6d4fa7e2edf3ec3432c785088084eb01b4b2d151
mauro_larese <mauro.larese@omnys.com>
2025-01-30 22:30:30 +0100

    add maven-wrapper.properties and improve output formatting in BookShelfRunner

--- src/main/java/com/example/test_git_flow/bookshelf/BookShelfRunner.java
+++ src/main/java/com/example/test_git_flow/bookshelf/BookShelfRunner.java
@@ -15,5 +15,6 @@ public class BookShelfRunner implements CommandLineRunner {
         System.out.println("--------------------------" );
         System.out.println("BookShelf: " + bookShelf);
         System.out.println("--------------------------" );
+        System.out.println("--------------------------" );
     }
 }
